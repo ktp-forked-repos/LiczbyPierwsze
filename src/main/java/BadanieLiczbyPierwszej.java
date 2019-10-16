36bf9896a3a875372d816b4bb8e53d66edab894c
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-16 12:23:11 +0200

    WpisywanieLiczbyZkonsoli

--- src/main/java/BadanieLiczbyPierwszej.java
+++ src/main/java/BadanieLiczbyPierwszej.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class BadanieLiczbyPierwszej {
+
+
+    public boolean czyLiczbaJestPierwsza(int liczba) {
+
+        if (liczba < 2) {
+            return false;
+        }
+        for (int i = 2; i <= liczba / 2; i++) {
+            if (liczba % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        BadanieLiczbyPierwszej liczbaDoSprawdzenia = new BadanieLiczbyPierwszej();
+        liczbaDoSprawdzenia.czyLiczbaJestPierwsza(scanner.nextInt());
+        System.out.println(liczbaDoSprawdzenia);
+
+
+    }
 }
